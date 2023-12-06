84381af73b9a52292e42768370acd74bba08ad29
Pósán Róbert <posanrobi@gmail.com>
2023-12-06 17:30:01 +0100

    http section done

---
+++ HTTP_Req/src/components/AvailablePlaces.jsx
@@ -0,0 +1,53 @@
+import Places from "./Places.jsx";
+
+import { useState, useEffect } from "react";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false); // noting state
+  const [availablePlaces, setAvailablePlaces] = useState([]); // data state
+  const [error, setError] = useState(); // error state
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch places, try again later.",
+        });
+        setIsFetching(false);
+      }
+    }
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
