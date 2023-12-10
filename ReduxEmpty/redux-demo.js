1283fa165214898cdfa0bc727e664a8d20265f5a
Pósán Róbert <posanrobi@gmail.com>
2023-12-10 15:55:00 +0100

    redux in nutshell

---
+++ ReduxEmpty/redux-demo.js
@@ -0,0 +1,31 @@
+//we need: Reducer() function, Store, Component, Action
+const redux = require("redux");
+
+// it"s a pure function - input: old state+action, output: new state
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
