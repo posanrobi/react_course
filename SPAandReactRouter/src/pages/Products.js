c1707f05d00b16b5626274b3a7d2fdc860ae1e79
Pósán Róbert <posanrobi@gmail.com>
2023-12-15 18:03:33 +0100

    SPA and router

---
+++ SPAandReactRouter/src/pages/Products.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
+function ProductsPage() {
+  return (
+    <>
+      <h1>My Products Page</h1>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={prod.id} relative="">
+              {prod.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ProductsPage;
